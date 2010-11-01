61ebba3c20fdae76be6d623905d19ab5f7e2086f
Juan María Martínez Arce <juan@insignia4u.com>
2010-11-01 14:57:44 -0300

    [Initial Import]: Add empty rails app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitacora::Application.config.secret_token = '6f2d04abde9cc99d68d537d07793db62d9263ad37bff6e0b1c4c60573f75e0c3307fa23416b234dedb7de500a7cedd00c985c03fc89a13d0fc8f6c1309ed762a'
