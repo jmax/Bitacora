3fe718c56c1bbf87891e401986670664a9774e08
Juan Maria Martinez Arce <jmax@scarlett.(none)>
2010-11-01 15:01:59 -0300

    [Rails Bootstrap]
      - Setup JQuery as default JavaScript Framework.

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module Bitacora
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery-1.4.2.min rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -40,3 +41,4 @@ module Bitacora
     config.filter_parameters += [:password]
   end
 end
+
